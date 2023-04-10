805bfb4a2c2f736af0bdad1e01547f22eed9bfaf
Grigoriy <grishavolkov999@gmail.com>
2023-04-10 23:44:29 +0300

    first_commit

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/Grigoriy/PycharmProjects/pythonProject3/shop.db'
+db = SQLAlchemy(app)
 
-@app.route('/', methods=['GET', 'POST'])
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    price = db.Column(db.Integer)
+
+
+@app.route('/')
 def index():
+    items = Product.query.order_by(Product.price).all()
+    return render_template('index.html', data=items)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+@app.route('/create', methods=["GET", "POST"])
+def create():
     if request.method == 'POST':
-        if 'button1' in request.form:
-            print('Куплено')
-        elif 'button2' in request.form:
-            print('Понравилось')
-    return render_template('index.html')
+        name = request.form['name']
+        price = request.form['price']
+        product = Product(name=name, price=price)
+        try:
+            db.session.add(product)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'ERROR'
+    else:
+        return render_template('create.html')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
